2d463a9b12dfe0eb43d20b3c1c9d0f1af2c90b1a
RaspberrYanagi <25424627+yana9i@users.noreply.github.com>
2021-03-02 12:37:17 +0800

    rebuild project

---
+++ build/precache-manifest.59870a2fccaed4f76ed90a31cb80a385.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fdde7e09a7b98886c9e41ca94382844",
+    "url": "./index.html"
+  },
+  {
+    "revision": "11e0b3397b29b678d3e5",
+    "url": "./static/css/2.9b25506c.chunk.css"
+  },
+  {
+    "revision": "b4eeeff2a6fe183fea39",
+    "url": "./static/css/main.c4a04843.chunk.css"
+  },
+  {
+    "revision": "11e0b3397b29b678d3e5",
+    "url": "./static/js/2.0e628c1a.chunk.js"
+  },
+  {
+    "revision": "7118f9d5a2d1eeb9f90535b650aafd94",
+    "url": "./static/js/2.0e628c1a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b4eeeff2a6fe183fea39",
+    "url": "./static/js/main.40a4f5aa.chunk.js"
+  },
+  {
+    "revision": "45906ca3eb4dbb7c83b6",
+    "url": "./static/js/runtime-main.c0cb87ad.js"
+  },
+  {
+    "revision": "b02bdc1b846fd65473922f5f62832108",
+    "url": "./static/media/element-icons.b02bdc1b.ttf"
+  },
+  {
+    "revision": "d2f69a92faa6fe990d2e613c358be705",
+    "url": "./static/media/element-icons.d2f69a92.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
